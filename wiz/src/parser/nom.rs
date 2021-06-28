f7db7e16523b6737bdbded526dbea899ae0cb494
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-06-28 23:57:52 +0900

    Update nom.rs

--- wiz/src/parser/nom.rs
+++ wiz/src/parser/nom.rs
@@ -11,15 +11,16 @@ use nom::character::complete::{digit1, space0, space1, one_of, char};
 use crate::ast::literal::Literal;
 use crate::ast::expr::Expr;
 use crate::ast::file::File;
-use crate::ast::stmt::{Stmt, AssignmentStmt};
+use crate::ast::stmt::{Stmt, AssignmentStmt, LoopStmt};
 use crate::ast::decl::Decl;
 use nom::sequence::tuple;
 use nom::combinator::map;
 use nom::branch::alt;
 use crate::parser::nom::expression::expr;
 use nom::multi::many0;
-use crate::parser::nom::declaration::decl;
-use crate::parser::nom::lexical_structure::{whitespace0, identifier};
+use crate::parser::nom::declaration::{decl, block};
+use crate::parser::nom::lexical_structure::{whitespace0, identifier, whitespace1};
+use crate::parser::nom::keywords::while_keyword;
 
 
 pub fn decl_stmt(s: &str) -> IResult<&str, Stmt> {
@@ -48,12 +49,34 @@ pub fn assignment_stmt(s: &str) -> IResult<&str, Stmt> {
     })(s)
 }
 
+pub fn loop_stmt(s: &str) -> IResult<&str, Stmt> {
+    map(alt((
+        while_stmt,
+        while_stmt,
+    )), |l| {
+        Stmt::Loop(l)
+    })(s)
+}
+
+pub fn while_stmt(s: &str) -> IResult<&str, LoopStmt> {
+    map(tuple((
+        while_keyword,
+        whitespace1,
+        expr,
+        whitespace1,
+        block,
+    )), |(_, _, e, _, b)| {
+        LoopStmt::While { condition: e, block: b }
+    })(s)
+}
+
 pub fn stmt(s: &str) -> IResult<&str, Stmt> {
     map(tuple((
         whitespace0,
         alt((
             decl_stmt,
             assignment_stmt,
+            loop_stmt,
             expr_stmt,
         ))
     )), |(ws, stm)| {
@@ -73,4 +96,30 @@ pub fn file(s: &str) -> IResult<&str, File> {
     ))), |decls| {
         File { body: decls.into_iter().map(|(_, f, _)| { f }).collect() }
     })(s)
+}
+
+mod tests {
+    use crate::parser::nom::while_stmt;
+    use crate::ast::stmt::{LoopStmt, Stmt, AssignmentStmt};
+    use crate::ast::block::Block;
+    use crate::ast::expr::Expr;
+    use crate::ast::literal::Literal;
+
+    #[test]
+    fn test_while_stmt() {
+        assert_eq!(while_stmt(
+            r"while (a < b) {
+            a = a + 1
+        }"), Ok(("", LoopStmt::While { condition: Expr::BinOp {
+            left: Box::new(Expr::Name { name: "a".to_string() }),
+            kind: "<".to_string(),
+            right: Box::new(Expr::Name { name: "b".to_string() })
+        }, block: Block { body: vec![
+            Stmt::Assignment(AssignmentStmt{ target: "a".to_string(), value: Expr::BinOp {
+                left: Box::new(Expr::Name { name: "a".to_string() }),
+                kind: "+".to_string(),
+                right: Box::new(Expr::Literal { literal: Literal::IntegerLiteral { value: "1".to_string() } })
+            } })
+        ] } })))
+    }
 }
