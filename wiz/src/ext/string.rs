a243e716f3bb4a646f87c641e1bb80927801a558
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-09-23 23:58:45 +0900

    Update string.rs

--- wiz/src/ext/string.rs
+++ wiz/src/ext/string.rs
@@ -1,18 +1,48 @@
 pub(crate) trait StringExt {
-    fn remove_first(&self) -> String;
-    fn remove_last(&self) -> String;
+    fn remove_first(&self) -> Self;
+    fn remove_last(&self) -> Self;
 }
 
 impl StringExt for String {
-    fn remove_first(&self) -> String {
-        let mut chars = self.chars();
-        chars.next();
-        String::from(chars.as_str())
+
+    fn remove_first(&self) -> Self {
+        let str: &str = self;
+        Self::from(str.remove_first())
     }
 
-    fn remove_last(&self) -> String {
+    fn remove_last(&self) -> Self {
+        let str: &str = self;
+        Self::from(str.remove_last())
+    }
+}
+
+impl StringExt for &str {
+    fn remove_first(&self) -> Self {
+        let mut chars = self.chars();
+        chars.next();
+        chars.as_str()
+    }
+
+    fn remove_last(&self) -> Self {
         let mut chars = self.chars();
         chars.next_back();
-        String::from(chars.as_str())
+        chars.as_str()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ext::string::StringExt;
+
+    #[test]
+    fn test_remove_first() {
+        assert_eq!("123456789", "0123456789".remove_first());
+        assert_eq!(String::from("123456789"), String::from("0123456789").remove_first());
+    }
+
+    #[test]
+    fn test_remove_last() {
+        assert_eq!("012345678", "0123456789".remove_last());
+        assert_eq!(String::from("012345678"), String::from("0123456789").remove_last());
     }
 }
