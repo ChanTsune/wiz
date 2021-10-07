b325baa0d3572a1b59a2579d8270572faec73548
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-10-07 20:33:13 +0900

    Update type_resolver.rs

--- wiz/src/high_level_ir/type_resolver.rs
+++ wiz/src/high_level_ir/type_resolver.rs
@@ -286,7 +286,6 @@ impl TypeResolver {
             }),
             TypedArgDef::Self_(_) => {
                 let self_type = self.context.get_current_type();
-                println!("Self => {:?}", self_type);
                 TypedArgDef::Self_(self_type)
             }
             TypedArgDef::RefSelf(_) => {
@@ -447,7 +446,6 @@ impl TypeResolver {
                 .into_iter()
                 .map(|a| {
                     let a = self.typed_arg_def(a)?;
-                    println!("a => {:?}", a);
                     let ns = self.context.get_current_namespace_mut()?;
                     ns.values.insert(
                         a.name(),
