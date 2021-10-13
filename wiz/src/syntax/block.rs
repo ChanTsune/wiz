65335d0504c12ce4d038e3f22afe244d5d3a8553
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-10-13 09:09:55 +0900

    Update block.rs

--- wiz/src/syntax/block.rs
+++ wiz/src/syntax/block.rs
@@ -11,4 +11,22 @@ pub struct BlockSyntax {
     pub(crate) close: TokenSyntax,
 }
 
+impl Syntax for BlockSyntax {
+    fn with_leading_trivia(self, trivia: Trivia) -> Self {
+        Self {
+            open: self.open.with_leading_trivia(trivia),
+            body: self.body,
+            close: self.close
+        }
+    }
+
+    fn with_trailing_trivia(self, trivia: Trivia) -> Self {
+        Self {
+            open: self.open,
+            body: self.body,
+            close: self.close.with_trailing_trivia(trivia)
+        }
+    }
+}
+
 impl SyntaxNode for BlockSyntax {}
