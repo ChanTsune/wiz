a207c7698316bb31d834d4610123a63df74da895
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-10-17 22:34:32 +0900

    Update name_space.rs

--- wiz/wiz_syntax/src/parser/wiz/name_space.rs
+++ wiz/wiz_syntax/src/parser/wiz/name_space.rs
@@ -45,3 +45,20 @@ pub fn name_space_element<I>(s: I) -> IResult<I, NameSpaceElementSyntax>
         }
     })(s)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::wiz::name_space::{name_space, name_space_element};
+    use crate::syntax::name_space::{NameSpaceElementSyntax, NameSpaceSyntax};
+
+    #[test]
+    fn test_name_space_element() {
+        assert_eq!(name_space_element("name::"), Ok(("", NameSpaceElementSyntax::from("name"))));
+    }
+
+    #[test]
+    fn test_name_space() {
+        assert_eq!(name_space(""), Ok(("", NameSpaceSyntax::default())));
+        assert_eq!(name_space("a::b::"), Ok(("", NameSpaceSyntax::from(vec!["a", "b"]))));
+    }
+}
