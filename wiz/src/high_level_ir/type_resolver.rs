72e8d0f2de127b973e8354d4e634d58892847acf
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-10-20 09:35:25 +0900

    Update type_resolver.rs

--- wiz/src/high_level_ir/type_resolver.rs
+++ wiz/src/high_level_ir/type_resolver.rs
@@ -217,6 +217,10 @@ impl TypeResolver {
             rs.member_functions.insert(member_function.name, type_);
         }
         for ini in initializers.iter() {
+            let type_ = self.context.full_type_name(TypedType::Function(Box::new(TypedFunctionType {
+                arguments: ini.args.clone(),
+                return_type: this_type.clone(),
+            })))?;
             let ns = self.context.get_current_namespace_mut()?;
             let rs = ns.get_type_mut(&name).ok_or(ResolverError::from(format!(
                 "Struct {:?} not exist. Maybe before preload",
@@ -224,10 +228,7 @@ impl TypeResolver {
             )))?;
             rs.static_functions.insert(
                 String::from("init"),
-                TypedType::Function(Box::new(TypedFunctionType {
-                    arguments: ini.args.clone(),
-                    return_type: this_type.clone(),
-                })),
+                type_,
             );
         }
 
