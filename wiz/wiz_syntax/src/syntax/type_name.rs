56d4619a4f34bc7616f20d7bd479a15deca866b6
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-10-23 22:49:55 +0900

    Update type_name.rs

--- wiz/wiz_syntax/src/syntax/type_name.rs
+++ wiz/wiz_syntax/src/syntax/type_name.rs
@@ -10,10 +10,45 @@ pub enum TypeName {
     Decorated(Box<DecoratedTypeName>),
 }
 
+impl Syntax for TypeName {
+    fn with_leading_trivia(self, trivia: Trivia) -> Self {
+        match self {
+            TypeName::NameSpaced(n) => TypeName::NameSpaced(Box::new(n.with_leading_trivia(trivia))),
+            TypeName::Simple(s) => TypeName::Simple(s.with_leading_trivia(trivia)),
+            TypeName::Decorated(d) => TypeName::Decorated(Box::new(d.with_leading_trivia(trivia))),
+        }
+    }
+
+    fn with_trailing_trivia(self, trivia: Trivia) -> Self {
+        match self {
+            TypeName::NameSpaced(n) => TypeName::NameSpaced(Box::new(n.with_trailing_trivia(trivia))),
+            TypeName::Simple(s) => TypeName::Simple(s.with_trailing_trivia(trivia)),
+            TypeName::Decorated(d) => TypeName::Decorated(Box::new(d.with_trailing_trivia(trivia))),
+        }
+    }
+}
+
+
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct NameSpacedTypeName {
     pub name_space: NameSpaceSyntax,
     pub type_name: TypeName,
+}
+
+impl Syntax for NameSpacedTypeName {
+    fn with_leading_trivia(self, trivia: Trivia) -> Self {
+        Self {
+            name_space: self.name_space.with_leading_trivia(trivia),
+            type_name: self.type_name
+        }
+    }
+
+    fn with_trailing_trivia(self, trivia: Trivia) -> Self {
+        Self {
+            name_space: self.name_space,
+            type_name: self.type_name.with_trailing_trivia(trivia)
+        }
+    }
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
@@ -22,19 +57,51 @@ pub struct SimpleTypeName {
     pub type_args: Option<Vec<TypeName>>,
 }
 
+impl Syntax for SimpleTypeName {
+    fn with_leading_trivia(self, trivia: Trivia) -> Self {
+        Self {
+            name: self.name.with_leading_trivia(trivia),
+            type_args: None
+        }
+    }
+
+    fn with_trailing_trivia(self, trivia: Trivia) -> Self {
+        match self.type_args {
+            None => {
+                Self {
+                    name: self.name.with_trailing_trivia(trivia),
+                    type_args: None
+                }
+            }
+            Some(type_args) => {
+                Self {
+                    name: self.name,
+                    type_args: Some(todo!())
+                }
+            }
+        }
+    }
+}
+
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct DecoratedTypeName {
     pub decoration: TokenSyntax,
     pub type_: TypeName,
 }
 
-impl Syntax for TypeName {
+impl Syntax for DecoratedTypeName {
     fn with_leading_trivia(self, trivia: Trivia) -> Self {
-        todo!()
+        Self {
+            decoration: self.decoration.with_leading_trivia(trivia),
+            type_: self.type_
+        }
     }
 
     fn with_trailing_trivia(self, trivia: Trivia) -> Self {
-        todo!()
+        Self {
+            decoration: self.decoration,
+            type_: self.type_.with_trailing_trivia(trivia)
+        }
     }
 }
 
@@ -57,11 +124,19 @@ impl TypeParameterListSyntax {
 
 impl Syntax for TypeParameterListSyntax {
     fn with_leading_trivia(self, trivia: Trivia) -> Self {
-        todo!()
+        Self {
+            open: self.open.with_leading_trivia(trivia),
+            elements: self.elements,
+            close: self.close
+        }
     }
 
     fn with_trailing_trivia(self, trivia: Trivia) -> Self {
-        todo!()
+        Self {
+            open: self.open,
+            elements: self.elements,
+            close: self.close.with_trailing_trivia(trivia)
+        }
     }
 }
 
@@ -73,11 +148,25 @@ pub struct TypeParameterElementSyntax {
 
 impl Syntax for TypeParameterElementSyntax {
     fn with_leading_trivia(self, trivia: Trivia) -> Self {
-        todo!()
+        Self {
+            element: self.element.with_leading_trivia(trivia),
+            trailing_comma: self.trailing_comma
+        }
     }
 
     fn with_trailing_trivia(self, trivia: Trivia) -> Self {
-        todo!()
+        match self.trailing_comma {
+            None =>
+                Self {
+                    element: self.element.with_trailing_trivia(trivia),
+                    trailing_comma: None
+                },
+            Some(trailing_comma) =>
+                Self {
+                    element: self.element,
+                    trailing_comma: Some(trailing_comma.with_trailing_trivia(trivia))
+                },
+        }
     }
 }
 
@@ -102,14 +191,10 @@ impl Syntax for TypeParam {
                 type_constraint: None,
             },
             Some(type_constraint) => {
-                todo!(
-                    r"
-                Self {{
+                Self {
                     name: self.name,
                     type_constraint: Some(type_constraint.with_trailing_trivia(trivia))
-                }}
-                "
-                )
+                }
             }
         }
     }
@@ -123,10 +208,16 @@ pub struct TypeConstraintSyntax {
 
 impl Syntax for TypeConstraintSyntax {
     fn with_leading_trivia(self, trivia: Trivia) -> Self {
-        todo!()
+        Self {
+            sep: self.sep.with_leading_trivia(trivia),
+            constraint: self.constraint
+        }
     }
 
     fn with_trailing_trivia(self, trivia: Trivia) -> Self {
-        todo!()
+        Self {
+            sep: self.sep,
+            constraint: self.constraint.with_trailing_trivia(trivia)
+        }
     }
 }
