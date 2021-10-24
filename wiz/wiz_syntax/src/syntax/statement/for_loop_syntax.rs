9b9db109ba50c4eaa400af6b218a0e8c7fd8510e
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-10-24 21:20:59 +0900

    Update for_loop_syntax.rs

--- wiz/wiz_syntax/src/syntax/statement/for_loop_syntax.rs
+++ wiz/wiz_syntax/src/syntax/statement/for_loop_syntax.rs
@@ -1,6 +1,8 @@
 use crate::syntax::block::BlockSyntax;
 use crate::syntax::expression::Expr;
+use crate::syntax::Syntax;
 use crate::syntax::token::TokenSyntax;
+use crate::syntax::trivia::Trivia;
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct ForLoopSyntax {
@@ -9,4 +11,26 @@ pub struct ForLoopSyntax {
     pub in_keyword: TokenSyntax,
     pub iterator: Expr,
     pub block: BlockSyntax,
+}
+
+impl Syntax for ForLoopSyntax {
+    fn with_leading_trivia(self, trivia: Trivia) -> Self {
+        Self {
+            for_keyword: self.for_keyword.with_leading_trivia(trivia),
+            values: self.values,
+            in_keyword: self.in_keyword,
+            iterator: self.iterator,
+            block: self.block
+        }
+    }
+
+    fn with_trailing_trivia(self, trivia: Trivia) -> Self {
+        Self {
+            for_keyword: self.for_keyword,
+            values: self.values,
+            in_keyword: self.in_keyword,
+            iterator: self.iterator,
+            block: self.block.with_trailing_trivia(trivia)
+        }
+    }
 }
