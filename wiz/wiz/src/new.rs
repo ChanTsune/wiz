d7b64f57ffe517bb88b1b13933f46c8cfa68afdb
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-10-25 16:10:43 +0900

    Add `ansi_term`

--- wiz/wiz/src/new.rs
+++ wiz/wiz/src/new.rs
@@ -1,9 +1,10 @@
 use clap::ArgMatches;
 use std::env::current_dir;
 use std::error::Error;
+use ansi_term::Color;
 
 pub(crate) fn new_command(name: &str, options: &ArgMatches) -> Result<(), Box<dyn Error>> {
     let current = current_dir()?;
-    println!("{}", current.display());
+    println!("{} project at {1}", Color::Green.bold().paint("Created"), current.display());
     Ok(())
 }
