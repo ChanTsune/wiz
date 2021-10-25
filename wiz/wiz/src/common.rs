c5dd1267cb004aa009a9816ed99b066d3a291e51
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-10-25 21:52:58 +0900

    Update common.rs

--- wiz/wiz/src/common.rs
+++ wiz/wiz/src/common.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Pointer};
+use std::fs::{create_dir_all, File};
+use std::io::{BufWriter, Write};
 use std::path::PathBuf;
 
 #[derive(Debug)]
@@ -20,5 +22,23 @@ pub(crate) fn create_project(path: &PathBuf, project_name: &str) -> Result<(), B
             path.display()
         ))));
     };
+    let mut path = path.clone();
+    path.push("Package.wiz");
+    let mut package_wiz = BufWriter::new(File::create(&path)?);
+    writeln!(package_wiz, r#"
+val package = Package.init(
+    name: {:?},
+)"#, project_name)?;
+    path.pop();
+
+    path.push("src");
+    create_dir_all(&path)?;
+    path.push("main.wiz");
+    let mut main_wiz = BufWriter::new(File::create(&path)?);
+    writeln!(main_wiz,
+             r#"
+fun main() {{
+    println("Hello world!")
+}}"#)?;
     Ok(())
 }
