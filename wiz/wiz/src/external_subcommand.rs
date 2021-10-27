72eaed2849f1367c236b4fddf6276f67c1fa7d95
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-10-27 09:49:24 +0900

    Update external_subcommand.rs

--- wiz/wiz/src/external_subcommand.rs
+++ wiz/wiz/src/external_subcommand.rs
@@ -6,12 +6,15 @@ use std::os::unix::process::CommandExt;
 use std::process::Command;
 
 pub(crate) fn try_execute(cmd: &str, options: &ArgMatches) -> Result<(), Box<dyn Error>> {
+    let mut args = vec![cmd];
+    args.extend(options.values_of("").unwrap_or_default());
     let executable = format!("wiz-{}{}", cmd, env::consts::EXE_SUFFIX);
-    println!("external command {} {:?}", executable, options);
+    println!("external command {} {:?}", executable, args);
     let mut current_exe_path = env::current_exe()?;
     let _ = current_exe_path.pop();
     current_exe_path.push(executable);
     let mut command = Command::new(current_exe_path.as_os_str());
+    command.args(args);
     let err = command.exec();
     let error = anyhow::Error::from(err).context(ProcessError::new(None));
     if let Some(perr) = error.downcast_ref::<ProcessError>() {
