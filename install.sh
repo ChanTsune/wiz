2f750b03e65c636d03c9ffd2ba44286afdb2b30f
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-10-29 17:02:00 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 WIZ_HOME=${WIZ_HOME:-"$HOME/.wiz"}
-INSTALL_DIR="$WIZ_HOME/bin"
+BIN_DIR="$WIZ_HOME/bin"
 
 echo "WIZ_HOME=$WIZ_HOME"
-echo "INSTALL_DIR=$INSTALL_DIR"
+echo "BIN_DIR=$BIN_DIR"
 
 main() {
-    mkdir -p $INSTALL_DIR
+    mkdir -p "$BIN_DIR"
     build_install "wiz"
     build_install "wizc"
-    echo "Installation completed at $INSTALL_DIR"
-    echo "Add $INSTALL_DIR to your PATH"
+    echo "Installation completed at $BIN_DIR"
+    echo "Add $BIN_DIR to your PATH"
     echo 'export WIZ_HOME=$HOME/.wiz'
     echo 'PATH="$WIZ_HOME/bin:$PATH"'
 }
@@ -20,8 +20,8 @@ build_install() {
     pushd .
     cd "wiz/$1"
     cargo build --release
-    cp "target/release/$1" "$INSTALL_DIR/$1"
-    popd    
+    cp "target/release/$1" "$BIN_DIR/$1"
+    popd
 }
 
 main
