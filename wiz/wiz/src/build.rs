e76721e3f76b3edde70fb8351845e9ef90891f96
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-11-01 14:53:09 +0900

    Update build.rs

--- wiz/wiz/src/build.rs
+++ wiz/wiz/src/build.rs
@@ -1,15 +1,26 @@
 use clap::ArgMatches;
 use std::env;
 use std::error::Error;
+use std::fs::create_dir_all;
+use std::path::{Path, PathBuf};
+use crate::error::CliError;
 
 pub(crate) fn build_command(_: &str, options: &ArgMatches) -> Result<(), Box<dyn Error>> {
     let wizc = "wizc";
     let mut args = vec![wizc];
     let target_dir = if let Some(target_dir) = options.value_of("target-dir") {
-        String::from(target_dir)
+        let d = PathBuf::from(target_dir);
+        if d.exists() && !d.is_dir() {
+            return Err(Box::from(CliError::from(format!("{} is not directory", d.display()))))
+        } else {
+            d
+        }
     } else {
-        String::from(env::current_dir().unwrap().to_str().unwrap())
+        let mut current_dir = env::current_dir()?;
+        current_dir.push("target");
+        current_dir
     };
-    args.extend(["--target-dir", &target_dir]);
+    args.extend(["--target-dir", &target_dir.to_str().unwrap()]);
+    create_dir_all(&target_dir)?;
     super::subcommand::execute(wizc, &args)
 }
