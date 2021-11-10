ee0bfc532813e3e9e8081a0734affc69596c2c46
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-11-10 23:57:58 +0900

    Update codegen.rs

--- wiz/wizc/src/llvm_ir/codegen.rs
+++ wiz/wizc/src/llvm_ir/codegen.rs
@@ -647,7 +647,13 @@ impl<'ctx> CodeGen<'ctx> {
     fn need_load(may_be_pointer: AnyTypeEnum<'ctx>, request_type: &MLValueType) -> bool {
         match may_be_pointer {
             AnyTypeEnum::PointerType(p) => match request_type {
-                MLValueType::Primitive(_) | MLValueType::Struct(_) => true,
+                MLValueType::Primitive(pv) => match pv {
+                    MLPrimitiveType::String |
+                    MLPrimitiveType::Noting |
+                    MLPrimitiveType::Unit => false,
+                    _ => true
+                },
+                MLValueType::Struct(_) => true,
                 MLValueType::Pointer(r) | MLValueType::Reference(r) => {
                     Self::need_load(p.get_element_type(), r)
                 }
