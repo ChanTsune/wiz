3ea5d874e5226f8f010c6294d64e50c45dae04d3
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-12-06 09:55:18 +0900

    fix CI

--- wiz/wizc/src/high_level_ir/typed_file.rs
+++ wiz/wizc/src/high_level_ir/typed_file.rs
@@ -1,5 +1,6 @@
 use crate::high_level_ir::typed_decl::TypedDecl;
 use crate::high_level_ir::typed_use::TypedUse;
+use std::cmp::Ordering;
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub enum TypedSourceSet {
@@ -10,9 +11,42 @@ pub enum TypedSourceSet {
     },
 }
 
+impl TypedSourceSet {
+    fn name(&self) -> &str {
+        match self {
+            TypedSourceSet::File(f) => &f.name,
+            TypedSourceSet::Dir { name, items: _ } => name,
+        }
+    }
+}
+
+impl Ord for TypedSourceSet {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.name().cmp(other.name())
+    }
+}
+
+impl PartialOrd for TypedSourceSet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.name().partial_cmp(other.name())
+    }
+}
+
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct TypedFile {
     pub(crate) name: String,
     pub(crate) uses: Vec<TypedUse>,
     pub(crate) body: Vec<TypedDecl>,
+}
+
+impl Ord for TypedFile {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.name.cmp(&other.name)
+    }
+}
+
+impl PartialOrd for TypedFile {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.name.partial_cmp(&other.name)
+    }
 }
