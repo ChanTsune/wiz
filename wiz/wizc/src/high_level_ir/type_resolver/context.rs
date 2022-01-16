8682c37b62ceecd627d5ee0ed0a042af1b3b6faf
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-01-16 23:27:40 +0900

    Update context.rs

--- wiz/wizc/src/high_level_ir/type_resolver/context.rs
+++ wiz/wizc/src/high_level_ir/type_resolver/context.rs
@@ -180,9 +180,7 @@ impl NameSpace {
 
 impl NameEnvironment {
     fn new() -> Self {
-        Self {
-            names: Default::default(),
-        }
+        Self::default()
     }
 
     fn use_values_from(&mut self, name_space: &NameSpace) {
@@ -657,10 +655,7 @@ mod tests {
     use crate::high_level_ir::type_resolver::context::{
         EnvValue, NameSpace, ResolverContext, ResolverStruct,
     };
-    use crate::high_level_ir::typed_type::{
-        Package, TypedNamedValueType, TypedPackage, TypedType, TypedValueType,
-    };
-    use std::collections::HashSet;
+    use crate::high_level_ir::typed_type::{TypedType};
 
     #[test]
     fn test_name_space() {
