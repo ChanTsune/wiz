af6c03b234bb1ceac8d627396f70be8da17f4187
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-01-30 23:48:02 +0900

    Update name_syntax.rs

--- wiz/wiz_syntax/src/syntax/expression/name_syntax.rs
+++ wiz/wiz_syntax/src/syntax/expression/name_syntax.rs
@@ -2,11 +2,21 @@ use crate::syntax::name_space::NameSpaceSyntax;
 use crate::syntax::token::TokenSyntax;
 use crate::syntax::trivia::Trivia;
 use crate::syntax::Syntax;
+use crate::syntax::type_name::TypeArgumentListSyntax;
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct NameExprSyntax {
     pub name_space: NameSpaceSyntax,
     pub name: TokenSyntax,
+}
+
+impl NameExprSyntax {
+    pub fn simple(name: TokenSyntax) -> Self {
+        NameExprSyntax {
+            name_space: NameSpaceSyntax::default(),
+            name,
+        }
+    }
 }
 
 impl Syntax for NameExprSyntax {
