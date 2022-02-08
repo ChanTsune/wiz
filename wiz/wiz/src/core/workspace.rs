d80f4572188779cb0a095c85c258f0b0caeb2f21
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-02-08 23:31:59 +0900

    Update workspace.rs

--- wiz/wiz/src/core/workspace.rs
+++ wiz/wiz/src/core/workspace.rs
@@ -2,10 +2,24 @@ use crate::constant::MANIFEST_FILE_NAME;
 use crate::core::error::CliError;
 use std::error::Error;
 use std::path::PathBuf;
+use crate::manifest::{self, Manifest};
 
 pub(crate) struct Workspace {
     pub(crate) cws: PathBuf,
     pub(crate) manifest_path: PathBuf,
+}
+
+impl Workspace {
+    pub(crate) fn get_manifest(&self) -> Result<Manifest, Box<dyn Error>> {
+        if self.manifest_path.exists() {
+            Ok(manifest::read(&self.manifest_path)?)
+        } else {
+            Err(Box::new(CliError::from(format!(
+                "Manifest file not found at {}",
+                self.manifest_path.display()
+            ))))
+        }
+    }
 }
 
 pub(crate) fn construct_workspace_from(cws: PathBuf) -> Result<Workspace, Box<dyn Error>> {
