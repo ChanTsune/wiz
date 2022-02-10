5280d0a13948d5e7db582bec1a539ca2248f123c
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-02-10 09:50:35 +0900

    Update core.rs

--- wiz/wiz/src/core.rs
+++ wiz/wiz/src/core.rs
@@ -1,8 +1,10 @@
+use std::collections::BTreeMap;
 use crate::constant::MANIFEST_FILE_NAME;
 use std::error::Error;
 use std::fs::{create_dir_all, File};
 use std::io::{BufWriter, Write};
 use std::path::Path;
+use crate::core::manifest::{Manifest, PackageInfo};
 
 pub mod error;
 pub mod manifest;
@@ -11,20 +13,15 @@ pub mod workspace;
 pub(crate) fn create_project(path: &Path, project_name: &str) -> Result<(), Box<dyn Error>> {
     let mut path = path.to_path_buf();
     path.push(MANIFEST_FILE_NAME);
-    let mut package_wiz = BufWriter::new(File::create(&path)?);
-    writeln!(
-        package_wiz,
-        r#"
-[package]
-name = {:?}
-version = "0.1.0"
-
-[dependencies]
-core = "0.0.0"
-std = "0.0.0"
-"#,
-        project_name
-    )?;
+    manifest::write(&path, &Manifest {
+        package: PackageInfo { name: project_name.to_string(), version: "0.1.0".to_string() },
+        dependencies: {
+            let mut map = BTreeMap::new();
+            map.insert("core".to_string(), "0.0.0".to_string());
+            map.insert("std".to_string(), "0.0.0".to_string());
+            map
+        },
+    })?;
     path.pop();
 
     path.push("src");
