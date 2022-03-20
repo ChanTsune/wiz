4799324924a016f5e9dc494a0a0d8fcc6157b7a5
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-03-20 16:14:54 +0900

    Update type_checker.rs

--- wiz/wizc/src/high_level_ir/type_checker.rs
+++ wiz/wizc/src/high_level_ir/type_checker.rs
@@ -1,13 +1,12 @@
 mod error;
 
 use crate::high_level_ir::type_checker::error::CheckerError;
-use crate::high_level_ir::typed_decl::{
-    TypedDecl, TypedExtension, TypedFun, TypedProtocol, TypedStruct, TypedVar,
-};
+use crate::high_level_ir::typed_decl::{TypedDecl, TypedExtension, TypedFun, TypedFunBody, TypedProtocol, TypedStruct, TypedVar};
 use crate::high_level_ir::typed_expr::TypedExpr;
 use crate::high_level_ir::typed_file::{TypedFile, TypedSourceSet};
-use crate::high_level_ir::typed_stmt::TypedStmt;
+use crate::high_level_ir::typed_stmt::{TypedAssignmentStmt, TypedBlock, TypedLoopStmt, TypedStmt};
 use wiz_session::Session;
+use crate::high_level_ir::typed_type::TypedType;
 
 #[derive(Debug)]
 pub struct TypeChecker<'s> {
@@ -53,7 +52,26 @@ impl<'s> TypeChecker<'s> {
         self.expression(&typed_variable.value)
     }
 
-    fn function(&mut self, typed_function: &TypedFun) {}
+    fn function(&mut self, typed_function: &TypedFun) {
+        if let Some(body) = &typed_function.body {
+            match body {
+                TypedFunBody::Expr(e) => {
+                    self.expression(e);
+                    if typed_function.return_type != e.type_() {
+                        self.session.emit_error(CheckerError::new(format!(
+                            "TypeMissMatchError: {:?} excepted return {:?}, but return {:?}",
+                            typed_function.name,
+                            typed_function.return_type,
+                            e.type_(),
+                        )));
+                    }
+                }
+                TypedFunBody::Block(b) => {
+                    self.block(b);
+                }
+            };
+        }
+    }
 
     fn struct_(&mut self, typed_struct: &TypedStruct) {}
 
@@ -61,7 +79,60 @@ impl<'s> TypeChecker<'s> {
 
     fn extension(&mut self, typed_extension: &TypedExtension) {}
 
-    fn statement(&mut self, typed_statement: &TypedStmt) {}
+    fn statement(&mut self, typed_statement: &TypedStmt) {
+        match typed_statement {
+            TypedStmt::Expr(e) => {self.expression(e)}
+            TypedStmt::Decl(d) => {self.decl(d)}
+            TypedStmt::Assignment(a) => {self.assignment_statement(a)}
+            TypedStmt::Loop(l) => {self.loop_statement(l)}
+        }
+    }
+
+    fn assignment_statement(&mut self, typed_assignment: &TypedAssignmentStmt) {
+        match typed_assignment {
+            TypedAssignmentStmt::Assignment(a) => {
+                if a.target.type_() != a.value.type_() {
+                    self.session.emit_error(CheckerError::new(format!(
+                        "TypeMissMatchError: assignment {:?}, into {:?}",
+                        a.value.type_(),
+                        a.target.type_(),
+                    )))
+                }
+            }
+            TypedAssignmentStmt::AssignmentAndOperation(a) => {
+                if a.target.type_() != a.value.type_() {
+                    self.session.emit_error(CheckerError::new(format!(
+                        "TypeMissMatchError: assignment {:?}, into {:?}",
+                        a.value.type_(),
+                        a.target.type_(),
+                    )))
+                }
+            }
+        }
+    }
+
+    fn loop_statement(&mut self, typed_loop_statement: &TypedLoopStmt) {
+        match typed_loop_statement {
+            TypedLoopStmt::While(w) => {
+                if !w.condition.type_().map(|t|t.is_boolean()).unwrap_or_else(||false) {
+                    self.session.emit_error(CheckerError::new(format!(
+                        "while condition must be boolean, but {:?}", w.condition.type_()
+                    )))
+                }
+                self.block(&w.block);
+            }
+            TypedLoopStmt::For(f) => {
+                f.iterator.type_();
+                self.block(&f.block);
+            }
+        }
+    }
+
+    fn block(&mut self, typed_block: &TypedBlock) {
+        typed_block.body.iter().for_each(|s|{
+            self.statement(s)
+        })
+    }
 
     fn expression(&mut self, typed_expr: &TypedExpr) {}
 }
