bc0c910b9060ca3d61ce1b7aebd44e9d0ad316a6
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-05-05 20:21:41 +0900

    Update name_environment.rs

--- wiz/wizc/src/high_level_ir/type_resolver/name_environment.rs
+++ wiz/wizc/src/high_level_ir/type_resolver/name_environment.rs
@@ -72,21 +72,13 @@ impl<'a> NameEnvironment<'a> {
         mut name_space: Vec<String>,
         type_name: &str,
     ) -> Option<&ResolverStruct> {
-        if name_space.is_empty() {
-            match self.local_names.get(type_name) {
-                Some((_, EnvValue::Type(rs))) => Some(rs),
-                _ => None,
-            }
-        } else {
-            let n = name_space.remove(0);
-            match (name_space.is_empty(), self.local_names.get(&n)) {
-                (false, Some((_, EnvValue::NameSpace(ns)))) => match ns.get(name_space) {
-                    Some(EnvValue::NameSpace(ns)) => ns.get_type(type_name),
-                    _ => None,
-                },
-                (true, Some((_, EnvValue::NameSpace(ns)))) => ns.get_type(type_name),
-                (_, _) => None,
-            }
+        let maybe_type_parameter = match self.local_names.get(type_name) {
+            Some((_, EnvValue::Type(rs))) => Some(rs),
+            _ => None,
+        };
+        match maybe_type_parameter {
+            None => self.arena.get_type(&name_space, type_name),
+            Some(tp) => Some(tp),
         }
     }
 
