7b33170c37bc633c59f8c5d28a4b0f9e7eac3a84
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-05-14 22:52:31 +0900

    Update codegen.rs

--- wiz/wizc/src/llvm_ir/codegen.rs
+++ wiz/wizc/src/llvm_ir/codegen.rs
@@ -233,14 +233,7 @@ impl<'ctx> CodeGen<'ctx> {
                 } else {
                     let f = fields
                         .into_iter()
-                        .map(|(_, e)| {
-                            let e_type = e.type_();
-                            let e = self.expr(e);
-                            BasicValueEnum::try_from(
-                                self.load_if_pointer_value(e, &e_type.into_value_type()),
-                            )
-                            .unwrap()
-                        })
+                        .map(|(_, e)| BasicValueEnum::try_from(self.expr(e)).unwrap())
                         .collect::<Vec<_>>();
                     struct_type.const_named_struct(&f)
                 };
