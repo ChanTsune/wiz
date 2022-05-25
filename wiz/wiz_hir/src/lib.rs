08bb364bd196490280a5d275eb60624f8679f2d7
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-05-25 23:22:42 +0900

    Update lib.rs

--- wiz/wiz_hir/src/lib.rs
+++ wiz/wiz_hir/src/lib.rs
@@ -6,12 +6,3 @@ pub mod typed_stmt;
 pub mod typed_type;
 pub mod typed_type_constraint;
 pub mod typed_use;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
