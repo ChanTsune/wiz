77e20e40c69669527a7bc103b4ae54dae481470c
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-05-28 14:32:33 +0900

    Update lib.rs

--- wiz/wiz_utils/src/lib.rs
+++ wiz/wiz_utils/src/lib.rs
@@ -3,12 +3,3 @@ mod stacked_hash_map;
 
 pub use ext::string::StringExt;
 pub use stacked_hash_map::StackedHashMap;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
