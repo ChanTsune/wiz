3d1d294b90907b4cb885c5b2e5f6a47970bd890f
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-07-06 23:12:10 +0900

    Update test.rs

--- wiz/wiz/src/test.rs
+++ wiz/wiz/src/test.rs
@@ -1,20 +1,10 @@
-use crate::core::dep::resolve_manifest_dependencies;
-use crate::core::load_project;
 use crate::core::Result;
+use crate::build;
 use clap::ArgMatches;
 
 pub(crate) const COMMAND_NAME: &str = "test";
 
 pub(crate) fn command(_: &str, options: &ArgMatches) -> Result<()> {
-    let manifest_path = options.value_of("manifest-path");
-
-    let another_std = options.value_of("std");
-
-    let ws = load_project(manifest_path)?;
-    println!("{:?}", ws);
-    let resolved_dependencies =
-        resolve_manifest_dependencies(&ws.cws, &ws.get_manifest()?, another_std)?;
-    println!("{:?}", resolved_dependencies);
-    todo!();
+    build::command(build::COMMAND_NAME, options)?;
     Ok(())
 }
