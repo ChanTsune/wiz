5be00fa97ba2b9b0c4d43c57c2b7bdafa701fc00
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-07-31 23:19:04 +0900

    Update declaration.rs

--- wiz/wizc/src/high_level_ir/type_resolver/declaration.rs
+++ wiz/wizc/src/high_level_ir/type_resolver/declaration.rs
@@ -1,6 +1,8 @@
+use std::borrow::Borrow;
 use crate::high_level_ir::declaration_id::DeclarationId;
 use crate::high_level_ir::type_resolver::context::{ResolverFunction, ResolverStruct};
 use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
 use wiz_hir::typed_annotation::TypedAnnotations;
 use wiz_hir::typed_type::TypedType;
 
@@ -38,7 +40,9 @@ impl DeclarationItem {
         entry.insert(id);
     }
 
-    pub fn get_child(&self, name: &str) -> Option<&HashSet<DeclarationId>> {
+    pub fn get_child<Q: ?Sized>(&self, name: &Q) -> Option<&HashSet<DeclarationId>>
+    where String: Borrow<Q>, Q: Hash + Eq
+    {
         self.children.get(name)
     }
 
