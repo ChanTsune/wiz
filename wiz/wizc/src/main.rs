0448b939b058bf7c91f972695b010510a630461e
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-10-02 14:25:53 +0900

    Update main.rs

--- wiz/wizc/src/main.rs
+++ wiz/wizc/src/main.rs
@@ -129,7 +129,7 @@ fn run_compiler(session: &mut Session) -> Result<()> {
 
     let std_mlir = std_hlir
         .into_iter()
-        .map(|w| hlir2mlir(w, &[], &arena, &session, false))
+        .map(|w| hlir2mlir(w, &[], &arena, session, false))
         .collect::<Result<Vec<_>>>()?;
 
     fs::create_dir_all(&mlir_out_dir)?;
@@ -140,7 +140,7 @@ fn run_compiler(session: &mut Session) -> Result<()> {
         })?;
     }
 
-    let mlfile = hlir2mlir(hlfiles, &std_mlir, &arena, &session, true)?;
+    let mlfile = hlir2mlir(hlfiles, &std_mlir, &arena, session, true)?;
 
     session.timer(&format!("write mlir `{}`", mlfile.name), |_| {
         let mut f = fs::File::create(mlir_out_dir.join(&mlfile.name))?;
