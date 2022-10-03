cf5ad121e0887df7be32155ad2568035e5d3ca7a
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-10-03 10:47:08 +0900

    Update external_subcommand.rs

--- wiz/wiz/src/external_subcommand.rs
+++ wiz/wiz/src/external_subcommand.rs
@@ -4,7 +4,7 @@ use std::env;
 use std::iter::FromIterator;
 
 pub(crate) fn try_execute(cmd: &str, options: &ArgMatches) -> Result<()> {
-    let args = Vec::from_iter(options.get_many::<&str>("").unwrap().copied());
+    let args = Vec::from_iter(options.get_many::<String>("").unwrap().map(AsRef::as_ref));
     let executable = format!("wiz-{}{}", cmd, env::consts::EXE_SUFFIX);
     super::subcommand::execute(&executable, &args)
 }
