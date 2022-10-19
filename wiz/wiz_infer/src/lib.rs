36a290804173d447b0e53886113b00252eb994b9
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-10-19 22:19:19 +0900

    Update lib.rs

--- wiz/wiz_infer/src/lib.rs
+++ wiz/wiz_infer/src/lib.rs
@@ -35,7 +35,7 @@ pub fn run(
 
 /// collect `namespace`, `type` and `use`
 fn collect_namespace_and_type(
-    parent_page: &mut Page,
+    page: &mut Page,
     name: &str,
     f: &FileSyntax,
     parent: &DeclarationId,
@@ -45,37 +45,46 @@ fn collect_namespace_and_type(
     let parent = arena
         .register_namespace(parent, name, TypedAnnotations::default())
         .unwrap();
-    let mut page = Page::empty();
     for item in f.body.iter() {
-        if let DeclKind::Use(u) = &item.kind {
-            page.uses.push(Use::new(
-                u.package_name
-                    .as_ref()
-                    .map(|i| i.names.iter().map(|it| it.name.token()).collect::<Vec<_>>())
-                    .unwrap_or_default(),
-                u.used_name.token(),
-                u.alias.as_ref().map(|it| it.name.token()),
-            ));
-        } else if let DeclKind::Struct(s) = &item.kind {
-            let name = s.name.token();
-            match s.struct_keyword.token().as_str() {
-                "struct" => arena.register_struct(&parent, &name, TypedAnnotations::default()),
-                "protocol" => arena.register_protocol(&parent, &name, TypedAnnotations::default()),
-                token => unreachable!("{}", token),
-            };
-        } else if let DeclKind::Module((name, body)) = &item.kind {
-            let mut child_page = Page::empty();
-            collect_namespace_and_type(
-                &mut child_page,
-                name,
-                body.as_ref().unwrap(),
-                &parent,
-                arena,
-                session,
-            )?;
-            page.pages.insert(name.clone(), child_page);
+        match &item.kind {
+            DeclKind::Var(_) => {}
+            DeclKind::Fun(_) => {}
+            DeclKind::Struct(s) => {
+                let name = s.name.token();
+                match s.struct_keyword.token().as_str() {
+                    "struct" => arena.register_struct(&parent, &name, TypedAnnotations::default()),
+                    "protocol" => {
+                        arena.register_protocol(&parent, &name, TypedAnnotations::default())
+                    }
+                    token => unreachable!("{}", token),
+                };
+            }
+            DeclKind::ExternC(_) => {}
+            DeclKind::Enum { .. } => {}
+            DeclKind::Module((name, body)) => {
+                let mut child_page = Page::empty();
+                collect_namespace_and_type(
+                    &mut child_page,
+                    name,
+                    body.as_ref().unwrap(),
+                    &parent,
+                    arena,
+                    session,
+                )?;
+                page.pages.insert(name.clone(), child_page);
+            }
+            DeclKind::Extension(_) => {}
+            DeclKind::Use(u) => {
+                page.uses.push(Use::new(
+                    u.package_name
+                        .as_ref()
+                        .map(|i| i.names.iter().map(|it| it.name.token()).collect::<Vec<_>>())
+                        .unwrap_or_default(),
+                    u.used_name.token(),
+                    u.alias.as_ref().map(|it| it.name.token()),
+                ));
+            }
         }
     }
-    parent_page.pages.insert(name.to_string(), page);
     Ok(())
 }
