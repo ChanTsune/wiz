b884c07b739b2037461241a8437c2f2a525f7114
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-10-28 11:46:50 +0900

    Update wiz.rs

--- wiz/wiz_syntax_parser/src/parser/wiz.rs
+++ wiz/wiz_syntax_parser/src/parser/wiz.rs
@@ -1,15 +1,12 @@
 use crate::parser::error::ParseError;
 use crate::parser::wiz::statement::file;
 use crate::parser::Span;
-use std::collections::BTreeMap;
-use std::fs;
 use std::fs::read_to_string;
 use std::path::Path;
 use wiz_result::Result;
 use wiz_session::ParseSession;
 use wiz_span::{get_line_offset, Location};
-use wiz_syntax::syntax::declaration::{DeclKind, DeclarationSyntax};
-use wiz_syntax::syntax::{FileSyntax, WizFile};
+use wiz_syntax::syntax::{WizFile};
 
 pub mod annotation;
 pub mod character;
@@ -65,71 +62,9 @@ pub fn read_book_from_path(
     path: &Path,
     name: Option<&str>,
 ) -> Result<WizFile> {
-    if path.file_name().unwrap().to_str().unwrap() == "lib.wiz" {
-        println!("session root: {}", path.display());
-        println!("name: {:?}, path: {}", name, path.display());
-        parse_from_file_path(session, path, name)
-    } else {
-        println!("name: {:?}, path: {}", name, path.display());
-        read_page_from_path(session, &path, name)
-    }
-}
-
-#[deprecated]
-fn read_page_from_path(session: &ParseSession, path: &Path, name: Option<&str>) -> Result<WizFile> {
-    Ok(if path.is_dir() {
-        let dir = fs::read_dir(path)?;
-        WizFile {
-            name: name
-                .or_else(|| path.file_stem().and_then(|p| p.to_str()))
-                .unwrap_or_default()
-                .to_string(),
-            syntax: FileSyntax {
-                leading_trivia: Default::default(),
-                body: dir
-                    .into_iter()
-                    .map(|d| read_page_from_path(session, &*d?.path(), None))
-                    .collect::<Result<Vec<_>>>()?
-                    .into_iter()
-                    .fold(BTreeMap::new(), |mut acc, value| {
-                        acc.entry(value.name.to_string())
-                            .or_insert_with(Vec::new)
-                            .push(value);
-                        acc
-                    })
-                    .into_iter()
-                    .map(|(k, mut v)| {
-                        if v.len() == 1 {
-                            v.remove(0)
-                        } else {
-                            let mut sb = WizFile {
-                                name: k,
-                                syntax: FileSyntax {
-                                    leading_trivia: Default::default(),
-                                    body: Default::default(),
-                                    trailing_trivia: Default::default(),
-                                },
-                            };
-                            for item in v.into_iter() {
-                                sb.syntax.body.extend(item.syntax.body);
-                            }
-                            sb
-                        }
-                    })
-                    .map(|i| {
-                        let WizFile { name, syntax } = i;
-                        DeclarationSyntax {
-                            annotations: None,
-                            kind: DeclKind::Module((name, Some(syntax))),
-                        }
-                    })
-                    .collect(),
-                trailing_trivia: Default::default(),
-            },
-        }
-    } else {
-        parse_from_file_path(session, path, name)?
-    })
+    println!("session root: {}", path.display());
+    println!("name: {:?}, path: {}", name, path.display());
+    parse_from_file_path(session, path, name)
 }
 
 fn get_error_location_src<P: AsRef<Path>>(
