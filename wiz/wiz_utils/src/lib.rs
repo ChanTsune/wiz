8ed2dad8572f4d48d4903542c3af32485c4c074b
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-12-04 13:43:47 +0900

    Add random::ascii_string

--- wiz/wiz_utils/src/lib.rs
+++ wiz/wiz_utils/src/lib.rs
@@ -2,6 +2,7 @@ mod ext;
 mod stacked_hash_map;
 pub mod topological_sort;
 pub mod utils;
+pub mod random;
 
 pub use ext::string::StringExt;
 pub use stacked_hash_map::StackedHashMap;
---
+++ wiz/wiz_utils/src/random.rs
@@ -0,0 +1,15 @@
+use rand::prelude::SliceRandom;
+
+const BASE_STR: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+pub fn ascii_string(length: usize) -> String {
+    let mut rng = &mut rand::thread_rng();
+    unsafe {
+        String::from_utf8_unchecked(
+            BASE_STR.as_bytes()
+                .choose_multiple(&mut rng, length)
+                .cloned()
+                .collect()
+        )
+    }
+}
