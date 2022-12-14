790211f3da7708c624d9c7536d7c0b991d84d9c3
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-12-14 18:39:50 +0900

    use matches! macro

--- wiz/wizc/src/llvm_ir/codegen.rs
+++ wiz/wizc/src/llvm_ir/codegen.rs
@@ -867,12 +867,7 @@ impl<'ctx> CodeGen<'ctx> {
     fn need_load(may_be_pointer: AnyTypeEnum<'ctx>, request_type: &MLValueType) -> bool {
         match may_be_pointer {
             AnyTypeEnum::PointerType(p) => match request_type {
-                MLValueType::Primitive(pv) => match pv {
-                    MLPrimitiveType::String | MLPrimitiveType::Noting | MLPrimitiveType::Unit => {
-                        false
-                    }
-                    _ => true,
-                },
+                MLValueType::Primitive(pv) => matches!(pv, MLPrimitiveType::String | MLPrimitiveType::Noting | MLPrimitiveType::Unit),
                 MLValueType::Struct(_) => true,
                 MLValueType::Pointer(r) | MLValueType::Reference(r) => match &**r {
                     MLType::Value(r) => Self::need_load(p.get_element_type(), r),
